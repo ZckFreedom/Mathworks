09e8dc9116a9e77f18cf0984f8c1938a8566ec97
e <zhu2010_546@163.com>
2019-12-05 12:18:28 +0800

    修改后的方法，应该可以再修改

--- Finite_fields&Signal_design/fundamental_tools/cui_work.py
+++ Finite_fields&Signal_design/fundamental_tools/cui_work.py
@@ -1,63 +1,84 @@
 import time
-from A_way_of_q_Field import *
 
 
 start_time = time.perf_counter()
 
 
-def T1_reflection(element, order):
-	if len(element.get_body().get_body()) < order - 1:
-		return 0
-	elif len(element.get_body().get_body()) == order - 1:
-		return element.get_body().get_body()[0].get_body()
-	elif len(element.get_body().get_body()) == order:
-		return element.get_body().get_body()[1].get_body()
+class field_list:
+	def __init__(self, element_list):
+		self._list = element_list
+	
+	def __add__(self, other):
+		add_list = []
+		for i in range(0, len(self._list)):
+			add_list.append((self._list[i] + other.get_list()[i]) % 3)
+		return field_list(add_list)
+	
+	def get_list(self):
+		return self._list
 
 
-def T2_reflection(element, order):
-	if len(element.get_body().get_body()) < order - 1:
-		return 0, 0
-	elif len(element.get_body().get_body()) == order - 1:
-		return 0, element.get_body().get_body()[0].get_body()
-	elif len(element.get_body().get_body()) == order:
-		return element.get_body().get_body()[0].get_body(), element.get_body().get_body()[1].get_body()
+def delete_zero(divisor_list):
+	while True:
+		if divisor_list[0] == 0:
+			divisor_list.pop(0)
+		else:
+			break
 
 
-def T1_T2_list(a_irreducible_ply, order):
+def list_mod(primitive_ply, element_ply):       # 得到给的元素的基表示形式
+	n = len(primitive_ply)-1
+	m = len(element_ply)
+	if m < n:
+		norm_list = [0] * (n-m)
+		norm_list += element_ply
+		return norm_list
+	else:
+		while m > n:
+			diply = primitive_ply + [0] * (m-n)
+			for i in range(m):
+				element_ply[i] = (element_ply[i] - diply[i]) % 3
+			delete_zero(element_ply)
+			m = len(element_ply)
+		if m < n:
+			return [0]*(n-m) + element_ply
+		else:
+			return element_ply
+
+
+def T1_T2_list(primitive_list):
+	n = len(primitive_list) - 1
 	T1_list = []
 	T2_list = []
-	for i in range(0, 3**order - 1):
+	for i in range(0, 3**n-1):
 		element_list = [1] + [0] * i
-		the_element = Field_q(element_list, a_irreducible_ply)
-		if T1_reflection(the_element, order) == 1:
-			T1_list.append(the_element)
-		c1, c2 = T2_reflection(the_element, order)
+		element_norm = list_mod(primitive_list, element_list)
+		if element_norm[1] == 1:
+			T1_list.append(element_norm)
+		c1, c2 = element_norm[0], element_norm[1]
 		if (c1 == 2 and c2 != 2) or (c1 != 2 and c2 == 2):
-			T2_list.append(the_element)
+			T2_list.append(element_norm)
 		elif c1 == 2 and c2 == 2:
-			T2_list.append(the_element)
-			T2_list.append(the_element)
+			T2_list.append(element_norm)
+			T2_list.append(element_norm)
 	return T1_list, T2_list
 
 
 def the_sum(ply_list):
-	ply = PolynomialsField(ply_list, 3)
 	n = len(ply_list) - 1
+	u_sum = field_list([0]*n)
 	u_index = 0
-	e_list = [0] * n
 	u_index_list = [0] * n
-	u_sum = Field_q([0], ply)
-	u_sum1 = Field_q([0], ply)
-	T1, T2 = T1_T2_list(ply, n)
-	
+	e_list = [0] * n
+	T1, T2 = T1_T2_list(ply_list)
 	for j in range(n):
-		a = ((n-3)//4) // (3 ** (n - 1 - j))
-		e_list[j] = a
-		
-	while u_index < 3 ** n:
+		number = ((n-4)//5) // (3 ** (n - 1 - j))
+		e_list[j] = number
+	
+	while u_index < 3**n:
 		for j in range(n):
-			a = u_index // (3 ** (n - 1 - j))
-			u_index_list[j] = a
+			number = u_index // (3 ** (n - 1 - j))
+			u_index_list[j] = number
 		
 		count_add = 0
 		for i in range(0, n):
@@ -65,22 +86,16 @@ def the_sum(ply_list):
 				count_add += 1
 			if e_list[i] == 1 and u_index_list[i] == 2:
 				count_add += 1
-		if u_index <= 3 ** (n - 1) - 1:
-			u_sum1 += T1[u_index]
-		else:
-			u_sum1 += T2[u_index - 3 ** (n - 1)]
 		if count_add == 0:
 			if u_index <= 3**(n-1) - 1:
-				u_sum += T1[u_index]
+				u_sum += field_list(T1[u_index])
 			else:
-				u_sum += T2[u_index - 3**(n-1)]
+				u_sum += field_list(T2[u_index - 3**(n-1)])
 		
 		u_index += 1
-	
-	print(u_index)
 	return u_sum
 
 
-h1 = [1, 1, 0, 1, 0, 0, 0, 1]
-print(the_sum(h1))
+h1 = [1, 0, 0, 1, 0, 0, 0, 0, 2]
+print(the_sum(h1).get_list())
 print(time.perf_counter() - start_time, "seconds")
